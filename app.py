6585338672a1f7066dcb867dac7ac530c5ee33b8
Nagaraj Bhat <nagarajbhat12@gmail.com>
2020-05-28 17:34:05 +1000

    added port 5000

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ import os
 @app.route('/')
 def hello_world():
 	return 'Hello, world!'
+
+if __name__ == "__main__":
+	 # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
