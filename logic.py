e0f9d35edb0bfb964ee8ddc0a07d5a34e2162f93
Nagaraj Bhat <nagarajbhat12@gmail.com>
2020-05-29 10:20:46 +1000

    changed bg to snowtop

--- logic.py
+++ logic.py
@@ -3,6 +3,8 @@ import random
 
 # this is for quotes.csv ->	
 data = pd.read_csv('data/quotes.csv')
+
+# this function returns a random author,quote from the csv
 def randomquote(data):
 	pos = random.randint(0,len(data['Author'])-1)
 	author = data['Author'][pos]
@@ -12,14 +14,16 @@ def randomquote(data):
 """
 #this is for inspquotes.csv ->
 
-data = pd.read_csv('data/inspquotes.csv',names=['quote','author','genre'])
-
+#data = pd.read_csv('data/n_quotes_all.csv',names=['AUTHOR','QUOTE','GENRE'],sep=';')
+data = pd.read_csv('data/inspquotes.csv',sep=';')
+print(data.head()
 def randomquote(data):
-	pos = random.randint(0,len(data['Author'])-1)
-	author = data['Author'][pos]
-	quote = data['Quote'][pos]
+	#pos = random.randint(0,len(data['AUTHOR'])-1)
+	pos=1
+	author = data['AUTHOR'][pos]
+	quote = data['QUOTE'][pos]
 	genre = data['GENRE'][pos]
-	return author,quote,genre
-
-print(randomquote(data))
+	return author,quote
 """
+#print(randomquote(data))
+#print(data.isnull().sum())
